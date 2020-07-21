0bcc05d1f9dc1aef1473e61206c0a988f7cbbe5b
Terver Aosu <37598493+truetechcode@users.noreply.github.com>
2020-07-21 16:56:59 +0100

    add bootstrap package for react

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import logo from '../logo.svg';
 import '../assets/App.css';
+import { Button, Form } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Control type="email" placeholder="Search Jobs" />
+            <Form.Text className="text-muted">
+              Do you need a better job.
+    </Form.Text>
+          </Form.Group>
+        </Form>
+        <Button variant="primary">Submit</Button>
       </header>
     </div>
   );
